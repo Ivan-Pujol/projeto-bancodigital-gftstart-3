77021edf172894138d406b9ca607db9e0fdf6b75
Ivan-Pujol <ivan.ivanovich@hotmail.com>
2022-01-31 15:34:54 -0300

    inclusão dos tipos de conta PF e PJ juntamente com os seus respectivos limites, refatorado o metodo de emprestimo

--- src/one/digitalinovation/Conta.java
+++ src/one/digitalinovation/Conta.java
@@ -5,6 +5,7 @@ package one.digitalinovation;
      por ser apenas um exemplo e um teste para os novos conhecimentos, foram configurados apenas
      dois requisitos para a obtenção do emprestimo (o cliente não pode estar negativo e o emprestimo
      máximo desse banco ser de apenas 5000 pessoa fisica e 10000 para pessoa juridica
+     foi instaurado também os limites das contas PF e PJ.
 */
 
 public abstract class Conta implements IConta {
@@ -17,16 +18,50 @@ public abstract class Conta implements IConta {
     protected double saldo;
     protected Cliente cliente;
     protected boolean saldoNegativo;
+    protected final double limitePF = 500, limitePJ = 2000;
+    protected double limite, limiteReferencia;
 
     public Conta(Cliente cliente) {
         this.agencia = Conta.AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
         this.cliente = cliente;
+        if (cliente.getTipo().equals("FISICA")){
+            limite = limiteReferencia = limitePF;
+            System.out.println("Conta PF com limite pré-aprovado de R$"+limite);
+        }
+        if (cliente.getTipo().equals("JURIDICA")){
+            limite = limiteReferencia = limitePJ;
+            System.out.println("Conta PJ com limite pré-aprovado de R$"+limite);
+        }
     }
 
     @Override
     public void sacar(double valor) {
-        saldo -= valor;
+        if (saldo>valor){
+            saldo -= valor;
+        }
+
+        if (saldo<valor && saldo>0 ){
+            if (saldo+limite>=valor){
+               saldo -= valor;
+               limite += saldo;
+            }else{
+                System.out.println("======================================================================");
+                System.out.println("Saldo+limite insuficientes para essa transacao, operacao nao realizada");
+                System.out.println("======================================================================");
+            }
+        }else if (saldo<=0 ){
+            if (limite>=valor){
+                saldo -= valor;
+                limite -= valor;
+            }else{
+                System.out.println("======================================================================");
+                System.out.println("Saldo+limite insuficientes para essa transacao, operacao nao realizada");
+                System.out.println("======================================================================");
+            }
+
+        }
+
         if (saldo<0){
             saldoNegativo=true;
         }else saldoNegativo=false;
@@ -34,7 +69,14 @@ public abstract class Conta implements IConta {
 
     @Override
     public void depositar(double valor) {
-        saldo += valor;
+        if (saldo>=0){
+            saldo += valor;
+        }else{
+            saldo += valor;
+            if (saldo>=0){
+                limite = limiteReferencia;
+            }else limite +=valor;
+        }
         if (saldo<0){
             saldoNegativo=true;
         }else saldoNegativo=false;
@@ -78,21 +120,21 @@ public abstract class Conta implements IConta {
     private boolean checarRegraDeNegocio(Cliente cliente, double valor){
         boolean aprovado = false;
         if (cliente.getTipo().equals("FISICA")&&valor<5001){
-            System.out.println("==============================================================");
-            System.out.println("Prezado cliente PF informamos que seu emprestimo foi aprovado.");
-            System.out.println("==============================================================");
+            System.out.println("======================================================================================");
+            System.out.println("Prezado cliente PF informamos que seu emprestimo no valor de R$"+valor+" foi aprovado.");
+            System.out.println("======================================================================================");
             aprovado = true;
 
         }else if (cliente.getTipo().equals("JURIDICA")&&valor<10001){
-            System.out.println("==============================================================");
-            System.out.println("Prezado cliente PJ informamos que seu emprestimo foi aprovado.");
-            System.out.println("==============================================================");
+            System.out.println("======================================================================================");
+            System.out.println("Prezado cliente PJ informamos que seu emprestimo no valor de R$"+valor+" foi aprovado.");
+            System.out.println("======================================================================================");
             aprovado = true;
         }else {
-            System.out.println("============================================");
-            System.out.println("Caro cliente problema com o valor pretendido");
+            System.out.println("======================================================================================");
+            System.out.println("Caro cliente pessoa "+cliente.getTipo()+" ha um problema quanto ao montante pretendido");
             System.out.println("Infelizmente seu emprestimo foi recusado.");
-            System.out.println("============================================");
+            System.out.println("======================================================================================");
         }
         return aprovado;
     }
@@ -100,7 +142,10 @@ public abstract class Conta implements IConta {
     public void tomarEmprestimo(Cliente cliente, double valor){
 
         if (!checarConta(this)){
-            if (checarRegraDeNegocio(cliente, valor)) System.out.println("Parabens!");
+            if (checarRegraDeNegocio(cliente, valor)){
+                System.out.println("Parabens!");
+                this.depositar(valor);
+            }
         }
     }
 
@@ -109,5 +154,7 @@ public abstract class Conta implements IConta {
         System.out.println(String.format("Agencia: %d", this.agencia));
         System.out.println(String.format("Numero: %d", this.numero));
         System.out.println(String.format("Saldo: %.2f", this.saldo));
+        System.out.println(String.format("Limite: %.2f", this.limite));
+
     }
 }
--- src/one/digitalinovation/Main.java
+++ src/one/digitalinovation/Main.java
@@ -2,20 +2,44 @@ package one.digitalinovation;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente ivan = new Cliente("Ivan","JURIDICA" );
-        //ivan.setNome("Ivan");
+        Cliente ivanPF = new Cliente("Ivan","FISICA" );
+        Cliente ivanPJ = new Cliente("Ivan","JURIDICA" );
 
-        Conta cc = new ContaCorrente(ivan);
-        Conta poupanca = new ContaPoupanca(ivan);
 
-        cc.depositar(1110);
-        cc.transferir(1110, poupanca);
+        Conta cp = new ContaPoupanca(ivanPF);
+        Conta cc = new ContaCorrente(ivanPJ);
+
+        cc.depositar(110);
+        cc.transferir(200, cp);
+        cc.sacar(100);
+        cc.sacar(100);
+        cc.depositar(290);
+
 
         cc.imprimirExtrato();
-        poupanca.imprimirExtrato();
+        cp.imprimirExtrato();
 
-        cc.tomarEmprestimo(ivan, 500);
-        poupanca.tomarEmprestimo(ivan, 20000);
+        cc.tomarEmprestimo(ivanPJ, 500);
+        cp.tomarEmprestimo(ivanPF, 20000);
+
+        cp.sacar(1650);
+        cc.sacar(2000);
+        cc.imprimirExtrato();
+        cc.sacar(100);
+        cp.tomarEmprestimo(ivanPF,5000);
+        cp.imprimirExtrato();
+
+        cc.depositar(1999);
+        cc.depositar(101);
+
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
+
+        cp.sacar(5700);
+        cp.imprimirExtrato();
+
+        cp.sacar(1);
+
 
 
 
